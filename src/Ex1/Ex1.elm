57366df395d2b947e7e4a7ff9d72e65beb0223af
Paul Brabban <paul.brabban@gmail.com>
2018-12-01 09:24:48 +0000

    ex1

---
+++ src/Ex1/Ex1.elm
@@ -0,0 +1,34 @@
+module Ex1.Ex1 exposing (..)
+
+import Ex1.Input exposing (problemInput)
+
+import Set
+
+type alias Frequency = Int
+type alias DeltaFrequency = Int
+
+parseInput : String -> List DeltaFrequency
+parseInput =
+  String.split "\n"
+  >> List.map String.toInt
+  >> List.map (Maybe.withDefault 0)
+
+part1answer = parseInput problemInput |> List.sum
+
+findFirstMatch : List DeltaFrequency -> List DeltaFrequency -> Set.Set Frequency -> Frequency -> Frequency
+findFirstMatch input remaining seen total =
+    case remaining of
+      [] -> findFirstMatch input input seen total
+      x::xs ->
+        let
+          nextTotal = total + x
+        in
+          if Set.member nextTotal seen
+            then nextTotal
+            else findFirstMatch input xs (Set.insert total seen) nextTotal
+
+part2answer =
+  let
+    input = parseInput problemInput
+  in
+    findFirstMatch input input Set.empty 0
