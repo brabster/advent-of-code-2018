23e2eb3b8d09bd2dda05b8abe8355b60188726f3
Paul Brabban <paul.brabban@gmail.com>
2018-12-05 19:56:45 +0000

    faster, plus part 2

--- src/Ex5/Ex5.elm
+++ src/Ex5/Ex5.elm
@@ -2,40 +2,52 @@ module Ex5.Ex5 exposing (..)
 
 import Ex5.Input exposing (..)
 
+import Array exposing (Array)
+import Regex
 import Debug
 
 reactsWith : Char -> Char -> Bool
 reactsWith a b = a /= b && Char.toUpper a == Char.toUpper b
 
-reactStep : List Char -> List Char -> List Char
-reactStep results list =
-  case list of
-    [] -> List.reverse results
-    [a] -> a :: results
-    a::b::cs ->
+reactStep : Array Char -> Int -> Array Char
+reactStep chars index =
+  if index > Array.length chars
+    then chars
+    else
       let
-        nextRest = if reactsWith a b
-          then cs
-          else b :: cs
-        nextResults = if reactsWith a b
-          then results
-          else a :: results
+        reacts = Maybe.map2 reactsWith (Array.get index chars) (Array.get (index + 1) chars)
       in
-        reactStep nextResults nextRest
+        if Maybe.withDefault False reacts
+          then Array.append (Array.slice 0 index chars) (Array.slice (index + 2) (Array.length chars) chars)
+          else reactStep chars (index + 1)
 
-fullyReact : List Char -> List Char
-fullyReact list =
+
+fullyReact : Array Char -> Array Char
+fullyReact chars =
   let
-    step1 = reactStep [] list
-    step2 = reactStep [] step1
-    dummy = Debug.log "step1" (List.length step1)
-    dummy2 = Debug.log "step2" (List.length step2)
+    step1 = reactStep chars 0
+    step2 = reactStep step1 0
+    --dummy = Debug.log "step1" (Array.length step1)
+    --dummy2 = Debug.log "step2" (Array.length step2)
   in
     if step1 == step2
       then step1
       else fullyReact step2
 
-part1answer = problemInput
-  |> String.toList
-  |> fullyReact
-  |> List.length
+reactString = String.toList >> Array.fromList >> fullyReact >> Array.length
+
+part1answer = reactString problemInput
+
+replaceRegex string re =
+  Regex.replace (Maybe.withDefault Regex.never (Regex.fromString re)) (\_ -> "") string
+
+typesToTry = String.toList "abcdefghijklmnopqrstuvwxyz"
+  |> List.map (
+    String.fromChar
+    >> (\s -> ["[", s, String.toUpper s, "]"])
+    >> String.join "")
+
+part2answer =
+  List.map
+    ((replaceRegex problemInput)
+    >> reactString) typesToTry
